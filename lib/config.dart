ead2ba99375a2be0be0368476df4b79bb3eb7d87
Weiyu <sunny2004412@gmail.com>
2025-11-15 04:40:14 +0800

    更新內容

--- lib/config.dart
+++ lib/config.dart
@@ -4,7 +4,7 @@ class Config {
 
   // ★★★ 之後 ngrok 網址變更，只要改這裡 ★★★
   // (這是我從 fan_control_page.dart 刪除的舊網址)
-  static const String baseUrl = "http://localhost:3000";
+  static const String baseUrl = "http://localhost:4000";
   
   // API 統一前綴
   static const String apiUrl = "$baseUrl/api";
